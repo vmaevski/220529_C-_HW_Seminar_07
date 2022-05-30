7a980e06a12a1bcd0580a9459648146bd589c7bb
vmaevski <vmaevski@mail.ru>
2022-05-30 23:03:14 +0700

    Task 3 completed.

---
+++ 220529_Task_3/Program.cs
@@ -0,0 +1,60 @@
+﻿// 3. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+void PrintColumnAverage(int[,] array)
+{
+    int row = array.GetLength(0);
+    int column = array.GetLength(1);
+    int sum = 0;
+    Console.WriteLine("Average:");
+    for (int j = 0; j < column; j++)
+    {
+        sum = 0;
+        for (int i = 0; i < row; i++)
+        {
+            sum += array[i, j];
+        } 
+        Console.Write($"{Math.Round((double)sum / row, 1)}" + "\t");
+    }
+    Console.WriteLine();
+}
+
+int[,] Random2DArray(int row, int column, int from, int to)
+{
+    int[,] array = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            array[i, j] = new Random().Next(from, to);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    int row = array.GetLength(0);
+    int column = array.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            Console.Write($"{array[i, j]}" + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите количество строк массива: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int column = int.Parse(Console.ReadLine());
+Console.Write("Введите диапазон значений от: ");
+int from = int.Parse(Console.ReadLine());
+Console.Write("Введите диапазон значений до: ");
+int to = int.Parse(Console.ReadLine());
+int[,] array = Random2DArray(row, column, from, to);
+Print2DArray(array);
+PrintColumnAverage(array);
